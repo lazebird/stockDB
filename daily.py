4841067a29247dac9b3ace67e4bf0b03d358c80a
lazebird <lazebird@gmail.com>
2024-11-02 14:26:19 +0800

    feat: more funcs

--- daily.py
+++ daily.py
@@ -5,7 +5,6 @@ import time
 import datetime
 from fs import load_data, write_data
 from stock import Stock
-from stocklist import StockList
 
 
 def stock_update(lst: list, n):
@@ -16,16 +15,17 @@ def stock_update(lst: list, n):
     lst.sort(key=lambda x: x["code"])
 
 
-def monthly_update(date: datetime.date = None, max=-1, interval=5):
+def daily_update(date: datetime.date = None, max=-1, interval=5):
     lst = load_data("list.txt", defval=[])
-    for e in StockList().lst[:max]:
+    for e in lst[:max]:
+        print(e)
         s = Stock(e["code"], e["name"], e["market"])
-        o = s.get_monthly(date)
-        stock_update(lst, o)
+        date = datetime.datetime.strptime(e["trade_date"], "%Y/%m/%d").date()
+        o = s.get_daily(start_date=date, end_date=date)
         time.sleep(interval)  # reduce speed to avoid server block
-    write_data(lst, "list.txt")
+        write_data(o, "{}/{}.txt".format(date.strftime("%Y%m%d"), s.code))
 
 
 if __name__ == "__main__":
     max = 4 if len(sys.argv) > 1 else -1
-    monthly_update(max=max)
+    daily_update(max=max)
--- stock.py
+++ stock.py
@@ -2,8 +2,8 @@ import sys
 
 sys.dont_write_bytecode = True
 import datetime
-import akshare as ak
 import time
+import akshare as ak
 
 
 class Stock:
@@ -19,28 +19,31 @@ class Stock:
             indicator = self.indicator = Stock.get_indicator(self.code, date)
 
         indicator["trade_date"] = datetime.datetime.strftime(indicator.get("trade_date", datetime.date(1970, 1, 1)), "%Y/%m/%d")
-        return {"code": self.code, "name": self.name, **indicator}
+        return {"code": self.code, "name": self.name, "market": self.market, **indicator}
 
-    def get_daily(self):
-        today = datetime.datetime.today()
-        self.hprice = Stock.get_hprice(self.code, start_date=(today - datetime.timedelta(days=1)).strftime("%Y%m%d"), end_date=today.strftime("%Y%m%d")).iloc[-1].to_dict()
-        self.fund_flow = Stock.get_fund_flow(self.code, self.market)
+    def get_daily(self, start_date: datetime.date = datetime.date.today(), end_date: datetime.date = datetime.date.today()):
+        hprice = self.hprice = Stock.get_hprice(self.code, start_date=start_date.strftime("%Y%m%d"), end_date=end_date.strftime("%Y%m%d")).to_dict()
+        fund_flow = self.fund_flow = Stock.get_fund_flow(self.code, self.market, end_date)
+        print(f"hprice={hprice}")
+        print(f"fund_flow={fund_flow}")
+        return {"code": self.code, "name": self.name, "market": self.market, **hprice, **fund_flow}
 
     def get_indicator(code, date: datetime.date = None):
         i = {}
-        indicators = None
+        dt = None
         try:
-            indicators = ak.stock_a_indicator_lg(symbol=code)
-            i = indicators.iloc[-1].to_dict() if date == None else indicators[indicators["trade_date"] == date].iloc[-1].to_dict()
+            dt = ak.stock_a_indicator_lg(symbol=code)
+            i = dt.iloc[-1].to_dict() if date == None else dt[dt["trade_date"] == date].iloc[-1].to_dict()
         except Exception as e:
-            print(f"Error: code {code}, date {date}, indicators {indicators}, errmsg {e}")
+            print(f"Error: code {code}, date {date}, indicators {dt}, errmsg {e}")
         return i
 
     def get_hprice(code, start_date="20050501", end_date="20050520"):
         return ak.stock_zh_a_hist(symbol=code, period="daily", start_date=start_date, end_date=end_date, adjust="qfq")
 
-    def get_fund_flow(code, market):
-        return ak.stock_individual_fund_flow(stock=code, market=market).iloc[-1].to_dict()
+    def get_fund_flow(code, market, date):
+        dt = ak.stock_individual_fund_flow(stock=code, market=market)
+        return dt.iloc[-1].to_dict() if date == None else dt[dt["trade_date"] == date].iloc[-1].to_dict()
 
     def __repr__(self) -> str:
         return f"{{{self.code, self.name, self.indicator, self.hprice, self.fund_flow}}}"
